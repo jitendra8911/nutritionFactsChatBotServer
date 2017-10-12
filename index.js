ec123b2fa2a517ab451d1819db0235eb2f51490f
Jitendra Malakalapalli <jmalakalapalli@egen.solutions>
2017-10-12 17:01:53 -0500

    fix quantity param

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ exports.nutritionFacts = functions.https.onRequest((request, response) => {
 
         console.log('API Request: ' + host + path);
 
-        let {body: {result: {parameters: {food}}}} = request;
+        let {body: {result: {parameters: {food,quantity,unit}}}} = request;
+        quantity = parseInt(quantity);
         if (food) {
             rp('https://api.edamam.com/api/food-database/parser?app_id=1f42d8bb&app_key=8ca5e6822e9abfd927289b214749ae7d&ingr=' + food)
                 .then(function (response) {
@@ -42,8 +43,8 @@ exports.nutritionFacts = functions.https.onRequest((request, response) => {
                             body: {
                                 ingredients: [
                                     {
-                                        quantity: 1,
-                                        measureURI: 'http://www.edamam.com/ontologies/edamam.owl#Measure_pound',
+                                        quantity: quantity || 1,
+                                        measureURI: 'http://www.edamam.com/ontologies/edamam.owl#Measure_' + unit || 'pound',
                                         foodURI: uri
 
                                     }
@@ -51,6 +52,8 @@ exports.nutritionFacts = functions.https.onRequest((request, response) => {
                             },
                             json: true
                         };
+                        console.log('options', options);
+                        console.log('options body', options.body.ingredients);
                         rp(options)
                             .then(function (response) {
                                 const nutrients = response['totalNutrients'];
@@ -61,11 +64,13 @@ exports.nutritionFacts = functions.https.onRequest((request, response) => {
                                     nutrients_labels.forEach(function(label) {
                                         message += nutrients[label]['label'] + ' ' + nutrients[label]['quantity'].toFixed(2) + nutrients[label]['unit'] + '\n';
                                     });
+                                    console.log('message', message);
                                     app.ask(app.buildRichResponse()
-                                        .addSimpleResponse('Nutrition facts of one pound of ' + food + ' are as follows')
+                                        .addSimpleResponse('nutrition facts of ' + quantity + ' ' + unit + ' of ' + 'food are as follows')
                                         .addSimpleResponse(message));
 
-                                    //app.ask(message);
+                                } else {
+                                    app.ask('sorry could not find, your search should be like one unit of egg or two liters of milk, etc');
                                 }
                             })
                             .catch(function (error) {
